ebe5846f7b16b6434a378694186ecd3cc4c10e23
MatthiasSch <Matthias.Schmerl@rwth-aachen.de>
2012-07-18 11:04:40 +0200

    minor changes

--- oricrete/folding/examples/ex06_line_pts.py
+++ oricrete/folding/examples/ex06_line_pts.py
@@ -305,7 +305,7 @@ def line_test_crane(n_steps = 10, dx = 1.0):
 
     return cp
 
-def rhombus_3x1_grab_points(n_steps = 10, dx = 1.0):
+def rhombus_3x1_crane(n_steps = 10, dx = 1.0):
     
     cp = CreasePattern(n_steps = n_steps, MAX_ITER = 500)
     
@@ -428,15 +428,15 @@ def rhombus_3x1_grab_points(n_steps = 10, dx = 1.0):
                     [(23, 0, 1.0), (27, 0, -1.0)],
                     [(23, 2, 1.0), (26, 2, -1.0)],
                     [(23, 2, 1.0), (27, 2, -1.0)],
-                    [(22, 0, 1.0), (23, 0, 1.0)],
+                    [(14, 1, 1.0), (16, 1, -1.0)],
                     
                     [(8, 2, 1.0)],
                     [(11, 1, 1.0)],
                     [(9, 2, 1.0)],
                     [(11, 0, 1.0)],
                     [(13, 0, 1.0), (14, 0, -1.0)],
+                    [(16, 1, 1.0), (18, 1, -1.0)],
                     [(2, 2, 1.0), (4, 2, -1.0)],
-                    [(15, 1, 1.0), (17, 1, -1.0)],
                     #[(13, 2, 1.0), (17, 2, -1.0)],
                     [(13, 1, 1.0), (15, 1, -1.0)]
                     ]
@@ -482,12 +482,11 @@ def rhombus_3x1_grab_points(n_steps = 10, dx = 1.0):
     X0[83] = 0.00016334
     X0[86] = 0.00025
     X0[89] = 0.00025
-    X0 *= 10
+    X0 *= 1
     
     np.set_printoptions(threshold='nan')
     print 'dR', cp.get_dR(X0)
     print 'R', cp.get_R(X0)
-    
     
     cp.set_next_node(X0)
 
@@ -510,9 +509,7 @@ if __name__ == '__main__':
 #    cp = halfcrane_2sticks(n_steps = 40)
 #    cp = halfcrane_2sticks_bar(n_steps = 40)
 #    cp = line_test_crane(n_steps = 40)
-    cp = rhombus_3x1_grab_points(n_steps = 80)
-
-
+    cp = rhombus_3x1_crane(n_steps = 80)
     # initialise View
     
     cpv = CreasePatternView(data = cp, show_cnstr = True)
