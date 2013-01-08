17aa55463ee4208cb2542946a9f263eb3355e10b
MatthiasSch <Matthias.Schmerl@rwth-aachen.de>
2013-01-08 21:00:13 +0100

    mesh refinement on work

--- oricrete/folding/abaqus_link.py
+++ oricrete/folding/abaqus_link.py
@@ -16,7 +16,7 @@ from etsproxy.traits.api import HasTraits, Property, cached_property, Event, \
     Str, Tuple, Interface, implements, Enum, List, Float, Dict, DelegatesTo
 
 from etsproxy.traits.ui.api import Item, View, HGroup, RangeEditor
-
+from copy import copy
 import numpy as np
 from scipy.optimize import fmin_slsqp
 import sys
@@ -25,7 +25,7 @@ class AbaqusLink(HasTraits):
     origin_nodes = Array(value = [], dtype = float, geometry = True)
     origin_facets = Array(value = [], dtype = 'int_', geometry = True)
     origin_cl = Array(value = [], dtype = 'int_', geometry = True)
-    n_split = Int(2, geometry = True)
+    n_split = Int(2, geometry = True) # Number of segments each creaseline will be splitt up
     
     nodes = Property
     def get_nodes(self):
@@ -35,30 +35,59 @@ class AbaqusLink(HasTraits):
     def get_facets(self):
         return self._geometry[1]
     
-    _geometry = Property(depends_on = '+geometry')
+    geometry = Property(depends_on = 'n_split')
     @cached_property
-    def _get__geometry(self):
-        L = 1 / self.n_split
+    def _get_geometry(self):
+        # distance of all new nodes in the facet
+        L = 1 / float(self.n_split)
+        print L
         inner_nodes = np.array([])
-        facets = np.array([])
         crease_line_nodes = np.array([])
-        for f in self.origin_facets:
-            #build inner nodes
-            #build inner facets 
-            pass
+        facets = np.array([])
+        
+        if(L < 0.5):
+            L2 = copy(L)
+            L3 = copy(L)
+            L_list = []
+            for i in range(1, self.n_split - 1):
+                    for j in range(1, self.n_split - i):
+                        L1 = 1 - (L2 * j + L3 * i)
+                        L_list.append([[L1 ],
+                                      [ L2 * j],
+                                      [ L3 * i]])
+            for f in self.origin_facets:
+                #build inner nodes
+                f_nodes = self.origin_nodes[f]
+                for L_node in L_list:
+                    temp_node = (f_nodes * L_node).sum(axis = 0)
+                    inner_nodes = np.append(inner_nodes, temp_node).reshape((-1, 3))
+                
+                print inner_nodes
+                #build inner facets 
+            
+        
         for cl in self.origin_cl:
             #build cl nodes
             #build outer facets
-            
+            pass
         #add origin nodes with new nodes
         
         return
 
 if __name__ == '__main__':
-    points = np.array([[0, 1, 0],
-                       [1, 1, 1],
-                       [0, 0, 1]])
+    points = np.array([[0, 0, 0],
+                       [1, 0, 0],
+                       [0.5, 1, 0]])
     
-    print points
-    index = points.tolist().index([1, 1, 1])
-    print index
+    L = np.array([[[0.2],
+                  [0.2],
+                  [0.6]]])
+    
+    print (L * points)
+    
+    facet = [[1, 2, 0]]
+    al = AbaqusLink(origin_facets = facet, origin_nodes = points)
+    al.n_split = 6
+    al.geometry
+    
+    
