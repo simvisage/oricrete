45ea8f7647ea723ad154d41809a5d66908a6aeae
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2013-11-25 11:13:40 +0100

    Constant length constraint cleaned up

--- oricrete/folding2/eq_cons_constant_length.py
+++ oricrete/folding2/eq_cons_constant_length.py
@@ -12,10 +12,9 @@
 #
 # Created on Nov 18, 2011 by: matthias
 
-from etsproxy.traits.api import HasTraits, Property, DelegatesTo, PrototypedFrom
+from etsproxy.traits.api import DelegatesTo, PrototypedFrom
 
 import numpy as np
-import sympy as sp
 
 from eq_cons import EqCons
 
@@ -46,18 +45,11 @@ class EqConsConstantLength(EqCons):
         ''' Calculate the residuum for constant crease length
         given the fold vector dX.
         '''
-        L_vectors = self.reshaping.cp.L_vectors
-
-        j = self.L[:, 1]
-        i = self.L[:, 0]
-
+        v_0 = self.reshaping.cp.L_vectors
         u = U.reshape(self.n_N, self.n_D)
-        u_j = u[j]
-        u_i = u[i]
-
-        v_u_i = np.sum(L_vectors * u_i, axis=1)
-        v_u_j = np.sum(L_vectors * u_j, axis=1)
-
+        u_i, u_j = u[self.L.T]
+        v_u_i = np.sum(v_0 * u_i, axis=1)
+        v_u_j = np.sum(v_0 * u_j, axis=1)
         u_ij = np.sum(u_i * u_j, axis=1)
         u_ii = np.sum(u_i ** 2, axis=1)
         u_jj = np.sum(u_j ** 2, axis=1)
@@ -68,29 +60,18 @@ class EqConsConstantLength(EqCons):
     def get_G_du(self, U, t):
         ''' Calculate the residuum for constant crease length
         given the fold vector dX.
-
         '''
-
-        L_vectors = self.reshaping.cp.L_vectors
-
-        i = self.L[:, 0]
-        j = self.L[:, 1]
-
-        u = U.reshape(self.n_N, self.n_D)
-        u_i = u[i]
-        u_j = u[j]
-
-        G_du_i = -2 * L_vectors + 2 * u_i - 2 * u_j
-        G_du_j = 2 * L_vectors + 2 * u_j - 2 * u_i
-
         G_du = np.zeros((self.n_L, self.n_N, self.n_D), dtype='float_')
 
         # running crease line index
         if self.n_L > 0:
-            cidx = np.arange(self.n_L)
-
-            G_du[ cidx, i, : ] += G_du_i
-            G_du[ cidx, j, : ] += G_du_j
+            v_0 = self.reshaping.cp.L_vectors
+            u = U.reshape(self.n_N, self.n_D)
+            i, j = self.L.T
+            u_i, u_j = u[self.L.T]
+            l = np.arange(self.n_L)
+            G_du[ l, i, : ] += -2 * v_0 + 2 * u_i - 2 * u_j
+            G_du[ l, j, : ] += 2 * v_0 - 2 * u_i + 2 * u_j
 
         # reshape the 3D matrix to a 2D matrix 
         # with rows for crease lines and columns representing 
@@ -100,3 +81,23 @@ class EqConsConstantLength(EqCons):
         G_du = G_du.reshape(self.n_L, self.n_N * self.n_D)
         return G_du
 
+if __name__ == '__main__':
+
+    from reshaping import Reshaping
+    from crease_pattern import CreasePattern
+
+    cp = CreasePattern(X=[[-4, -5, -3],
+                          [0, 0.0, 0],
+                          [1.0, 0.1, 0],
+                          ],
+                       L=[[0, 1], [1, 2], [2, 0]],
+                       )
+
+    reshaping = Reshaping(cp=cp)
+    constant_length = EqConsConstantLength(reshaping)
+
+    U = np.zeros_like(cp.X)
+    U[2] += 1.0
+
+    print [constant_length.get_G(U, 0)]
+    print [constant_length.get_G_du(U, 0)]
