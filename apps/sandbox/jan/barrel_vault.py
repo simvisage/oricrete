cf3d7413232484d3fc3c976e92796016cde372d8
jvanderwoerd <jvanderwoerd@IMB-125.imb.rwth-aachen.de>
2015-09-11 18:20:23 +0200

    Simulation of the realized barrel vault Studies on hp-Shell with target face

---
+++ apps/sandbox/jan/barrel_vault.py
@@ -0,0 +1,67 @@
+'''
+Created on 18.03.2015
+
+@author: jvanderwoerd
+'''
+from oricrete.folding2 import \
+    YoshimuraCreasePattern, fix, link, \
+    Initialization, CnstrTargetFace, r_, s_, t_, Folding, \
+    CreasePatternView
+import numpy as np
+
+from oricrete.folding2.abaqus_link import AbaqusLink
+
+
+def get_constrained_YCP(L_x, L_y, n_x, n_y, d):
+    ycp = YoshimuraCreasePattern(L_x=L_x, L_y=L_y, n_x=n_x, n_y=n_y)
+#test
+    fixed_node = fix(ycp.N_h[0, 0], (0, 1, 2))
+    planar_front_boundary = link(ycp.N_h[0, 0], 1, 1.0,
+                                 ycp.N_h[1:, 0], 1, -1.0)
+    planar_back_boundary = link(ycp.N_h[0, -1], 1, 1.0,
+                                 ycp.N_h[1:, -1], 1, -1.0)
+    linked_left_boundary_x = link(ycp.N_h[0, 0], 0, 1.0,
+                                  ycp.N_h[0, 1:], 0, -1.0)
+    linked_left_boundary_z = link(ycp.N_h[0, 0], 2, 1.0,
+                                  ycp.N_h[0, 1:], 2, -1.0)
+    linked_left_and_right_z = link(ycp.N_v[0, :], 2, 1.0,
+                                   ycp.N_v[1, :], 2, -1.0)
+    linked_right_boundary_x = link(ycp.N_v[-1, 0], 0, 1.0,
+                                   ycp.N_v[-1, 1:], 0, -1.0)
+    cntrl_displ = [([(ycp.N_h[-1, 1], 0, 1.0)], d)]
+
+    caf = CnstrTargetFace(F=[r_, s_, 4 * 0.4 * t_ * r_ * (1 - r_ / L_x) + 0.000015])
+    n_arr = np.hstack([ycp.N_h[:, :].flatten(),
+                       ycp.N_i[:, :].flatten()
+                       ])
+
+    init = Initialization(cp=ycp, tf_lst=[(caf, n_arr)])
+
+    lift = Folding(source=init, n_steps=10, MAX_ITER=500,
+                   goal_function_type='none',
+                   dof_constraints=fixed_node +
+                                   planar_front_boundary +
+                                   planar_back_boundary +
+                                   linked_left_boundary_x +
+                                   linked_left_boundary_z +
+                                   linked_left_and_right_z +
+                                   linked_right_boundary_x +
+                                   cntrl_displ,
+#                    tf_lst=[(caf, n_arr)]
+                   )
+    print 'u', lift.u_t[-1]
+    return init,lift
+
+'''configure parameters:'''
+
+init,fold = get_constrained_YCP(L_x=2.42, L_y=3.0,
+                           n_x=4, n_y=10, d= -0.28)#l_x length, l_y length, n_x number of elments, n_y number of Elements, d deformation of the right side
+
+
+v = CreasePatternView(root=init)
+v.configure_traits()
+al = AbaqusLink(data = fold, n_split = 4)
+al.model_name = 'barrel_vault'
+al.build_inp()
+
+
---
+++ apps/sandbox/jan/hp_shell_tgf.py
@@ -0,0 +1,117 @@
+'''
+Created on 06.07.2015
+
+@author: jvanderwoerd
+'''
+#-------------------------------------------------------------------------
+#
+# Copyright (c) 2012, IMB, RWTH Aachen.
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in simvisage/LICENSE.txt and may be redistributed only
+# under the conditions described in the aforementioned license.  The license
+# is also available online at http://www.simvisage.com/licenses/BSD.txt
+#
+# Thanks for using Simvisage open source!
+#
+# Created on Sep 8, 2011 by: matthias
+
+from etsproxy.traits.api import HasTraits, Float
+import numpy as np
+import sympy as sm
+a_, b_, c_, d_ = sm.symbols('a,b,c,d')
+
+
+# own Modules
+from oricrete.folding import \
+    YoshimuraCreasePattern, CreasePatternView, x_
+
+from oricrete.folding.cnstr_target_face import \
+    CnstrTargetFace, r_, s_, t_
+
+from oricrete.folding2.abaqus_link import AbaqusLink
+
+
+if __name__ == '__main__':
+
+    L_x = 2.42
+    L_y = 3.01
+    cp = YoshimuraCreasePattern(n_steps=8,
+                                L_x=L_x,
+                                L_y=L_y,
+                                n_x=4,
+                                n_y=20,
+                                show_iter=False,
+                                z0_ratio=0.1,
+                                MAX_ITER=100)
+    n_h = cp.N_h
+    n_v = cp.N_v
+    n_i = cp.N_i
+    
+    A = 0.16   #0.1
+
+    B = 0.1   #0.05
+
+    s_term = 4 * B * t_ * s_ * (1 - s_ / L_y)  # * r_ / L_x
+
+    face_z_t = CnstrTargetFace(
+        F=[r_, s_, - 4 * A * t_ * r_ * (1 - r_ / L_x) + s_term])
+    n_arr = np.hstack([n_h[:, :].flatten(),
+                       #n_v[:, :].flatten(),
+                       n_i[:, :].flatten()
+                       ])
+    cp.tf_lst = [(face_z_t, n_arr)]
+
+    cp.cnstr_lhs = [  # [(n_h[1, 0], 0, 1.0)], # 0
+        #                   [(n_h[0, -1], 0, 1.0)], # 1
+        [(n_h[1, -1], 1, 1.0), (n_h[1, 0], 1, 1.0)],
+    ]
+
+    cp.cnstr_rhs = np.zeros((len(cp.cnstr_lhs),), dtype=float)
+
+    # @todo - renaming of methods
+    # @todo - projection on the caf - to get the initial vector
+    # @todo - gemetry transformator
+    # @todo - derivatives of caf for the current position.
+    # @todo - rthombus generator with cut-away elements
+    # @todo - time step counting - save the initial step separately from the time history
+
+    X0 = cp.generate_X0()
+    
+
+    
+    X_fc = cp.solve(X0 + 1e-6)
+
+ 
+    print 'nodes', cp.get_new_nodes(X_fc)
+
+    #
+#    print 'nodes'
+#    new_nodes = cp.get_new_nodes(X_fc)
+#    cp2 = CreasePattern(nodes = new_nodes,
+#                        crease_lines = cp.crease_lines,
+#                        facets = cp.facets,
+#                        n_steps = 1,
+#                        show_iter = True,
+#                        z0_ratio = 0.1,
+#                        MAX_ITER = 200)
+#
+#    face_z_t = CnstrTargetFace(F = [r_, s_, 0])
+#
+#    cp2.tf_lst = [(face_z_t, n_arr)]
+#
+#    cp2.cnstr_lhs = [[(n_h[1, 0], 0, 1.0)], # 0
+# #                       [(n_h[1, -1], 0, 1.0)], # 1
+# #                    [(n_h[1, -1], 1, 1.0), (n_h[1, 0], 1, 1.0)],
+#                    ]
+#    cp2.cnstr_rhs = np.zeros((len(cp2.cnstr_lhs),), dtype = float)
+#
+#    X0 = -1e-3 * np.linalg.norm(X_fc) * X_fc
+#
+#    X_fc = cp2.solve(X0, constant_length = True)
+#
+    my_model = CreasePatternView(data=cp,
+                                 ff_resolution=30,
+                                 show_cnstr=True)
+    my_model.configure_traits()
